160af3de1d4d4b455d58e4a4baea483f8d76b95c
GrantM <grantmarais@gmail.com>
2017-08-19 13:48:47 +0200

    Test runner clean up

--- src/test/java/entelect/bdd/pos/exercise1/CheckoutCucumberTestRunner.java
+++ src/test/java/entelect/bdd/pos/exercise1/CheckoutCucumberTestRunner.java
@@ -2,10 +2,7 @@ package entelect.bdd.pos.exercise1;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:entelect/bdd/pos/exercise1/checkout.feature")
